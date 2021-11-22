2c7d449c068afff12de0d3b74341a3de1110fb09
vivek kumar <57774129+Vivek-jpg@users.noreply.github.com>
2021-11-22 18:08:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const Btn = document.querySelector('.btn');
+const Output = document.querySelector('.output');
+Btn.addEventListener("click", fetchapi);
+
+function fetchapi() {
+  console.log("yes");
+
+
+  fetch("https://randomuser.me/api/?results=10")
+    .then(res => res.json())
+    .then(show => {
+      //console.log(show.results)
+      for (var i = 0; i < show.results.length; i++) {
+
+        var sunny = show.results[i].gender;
+        var firstname = show.results[i].name.first;
+       
+        if (sunny == "female") {
+          console.log("its working");
+         Output.innerHTML += `<li style = "color:green"> ${firstname + "    " + sunny}</li>`;
+        }
+        else{
+           Output.innerHTML += `<li style = "color:red"> ${firstname + "    " + sunny}</li>`;
+        }
+
+
+
+        // Output.innerText += firstname +show.results[i].gender;
+
+
+      }
+
+
+    })
+}
